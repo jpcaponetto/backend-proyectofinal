2fa478b53df8d36b5dc0960c05c868db4ddbb300
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2024-03-25 19:02:16 -0300

    Añadiendo mongosingleton

---
+++ src/db/mongo.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { getLogger } from "../utils/logger.js";
+import { envi } from "../../env.js";
+import { DBConnected, DBError } from "../utils/messages.js";
+
+export default class MongoSingleton {
+  static logger = getLogger();
+  constructor() {
+    const { URI } = envi.mongo;
+    const MONGO_URI = URI;
+
+    try {
+      this.connection = mongoose.connect(MONGO_URI);
+      DBConnected();
+    } catch (error) {
+      DBError(error);
+    }
+  }
+
+  static getInstance() {
+    if (!MongoSingleton.instance) {
+      MongoSingleton.instance = new MongoSingleton();
+    }
+    return MongoSingleton.instance;
+  }
+}
