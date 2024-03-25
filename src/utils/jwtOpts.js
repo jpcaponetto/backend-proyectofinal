a258d4786281ab852a8337dc26f6a8cc0374d6bc
Juan Pablo Caponetto <pablito__cs@hotmail.com>
2024-03-25 19:35:07 -0300

    Añadiendo Passport y jwtstrategy

---
+++ src/utils/jwtOpts.js
@@ -0,0 +1,16 @@
+import { envi } from "../../env.js";
+
+const SECRET_JWT = process.env.SECRET_JWT || envi.jwt.secret;
+
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.signedCookies) {
+    token = req.signedCookies.token;
+  }
+  return token;
+};
+
+export const jwtOpts = {
+  secretOrKey: SECRET_JWT,
+  jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+};
